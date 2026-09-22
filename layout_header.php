2e50fbdb05b7ba9b04ff0b96ffa23d7ab9b70f3a
Dracomoth <carlos.portillo@gmail.com>
2026-09-22 22:52:44 +0200

    Style changes

--- layout_header.php
+++ layout_header.php
@@ -28,7 +28,7 @@ try {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= htmlspecialchars($pageTitle) ?> | Videogame Vault</title>
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/style.css?v=<?= file_exists(__DIR__ . '/css/style.css') ? filemtime(__DIR__ . '/css/style.css') : '1.0' ?>">
 </head>
 <body>
 
